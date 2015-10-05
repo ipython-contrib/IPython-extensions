e18e0fcaffec4cb36f3bbaf1aa7be365c179be8b
Jan Schulz <jasc@gmx.net>
2015-10-05 10:49:34 +0200

    inactive: Small changes to doc
    Also add the __init__.py to let setup find the extension...

--- ipyext/inactive.py
+++ ipyext/inactive.py
@@ -1,22 +1,8 @@
 # encoding: utf-8
-"""
-Does *not* execute the cell ("inactive"). Usefull to temporary disable a cell.
 
-Authors:
-
-* Jan Schulz
-"""
-
-#-----------------------------------------------------------------------------
-#  Copyright (C) 2013  The IPython Development Team
-#
-#  Distributed under the terms of the BSD License.  The full license is in
-#  the file COPYING, distributed as part of this software.
-#-----------------------------------------------------------------------------
-
-#-----------------------------------------------------------------------------
-# Imports
-#-----------------------------------------------------------------------------
+# Copyright (C) 2013  The IPython Development Team
+# Copyright (c) IPython-extensions Development Team. 
+# Distributed under the terms of the Modified BSD License. 
 
 from IPython.core.magic import (Magics, magics_class, cell_magic)
 from IPython.testing.skipdoctest import skip_doctest
@@ -24,18 +10,29 @@ from IPython.core.error import UsageError
 
 @magics_class
 class InactiveMagics(Magics):
-    """Magic to *not* execute a cell."""
+    """Magic to *not* execute a cell.
+    
+    Useful for temporary deactivating a cell.    
+    """
 
     @skip_doctest
     @cell_magic
     def inactive(self, parameter_s='', cell=None):
-        """Does *not* exeutes a cell.
+        """Magic to *not* execute a cell.
         
-        Usage:
-          %%inactive
-          code...
-                
         This magic can be used to mark a cell (temporary) as inactive.
+        
+        Examples:
+        ---------
+        ::
+        
+            In [1]: %load_ext ipyext.inactive
+            'inactive' magic loaded.
+
+            In [2]: %%inactive
+               ...: print("code not run...")
+               ...:
+            Cell inactive: not executed!
         """
         if cell is None:
             raise UsageError('empty cell, nothing to ignore :-)')
