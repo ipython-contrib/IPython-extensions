1c82059098403203f69c222133b9558ab6f7ea20
Jan Schulz <jasc@gmx.net>
2015-10-05 15:10:29 +0200

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+pjoin = os.path.join
+repo_root = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    execfile
+except NameError:
+    def execfile(fname, globs, locs=None):
+        locs = locs or globs
+        exec(compile(open(fname).read(), fname, "exec"), globs, locs)
+
+
+try:
+    # make this optional, but it's better for PyPI uploads
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    with open('README.md', 'r') as f:
+        long_description = f.read()
+
+# _version.py contains __version__.
+execfile(pjoin(repo_root, 'ipyext','_version.py'), globals())
+        
+        
+setup(
+    name='IPython-extensions',
+    version=__version__,
+    license='BSD',
+    description='Extensions for IPython',
+    long_description=long_description,
+    author='IPython-extensions Development Team',
+    author_email='jupyter@googlegroups.org',
+    url='https://github.com/ipython-contrib/IPython-extensions',
+    packages = find_packages(exclude=['*test*']),
+    install_requires=['ipython'],
+    setup_requires=["numpydoc", "sphinx"]
+)
