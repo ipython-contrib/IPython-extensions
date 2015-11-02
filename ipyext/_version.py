05b9bea850244a5711bbc66b5482097580f33141
Jan Schulz <jasc@gmx.net>
2015-11-02 12:22:39 +0100

    Back to dev

--- ipyext/_version.py
+++ ipyext/_version.py
@@ -4,10 +4,10 @@
 # This is the only place were the release number must be adjusted
 _version_major = 0
 _version_minor = 1
-_version_patch = 0
+_version_patch = 99
 _version_extra = 'dev'
 # _version_extra = 'rc1'
-_version_extra = ''  # Uncomment this for full releases
+#_version_extra = ''  # Uncomment this for full releases
 
 # Construct full version string from these.
 _ver = [_version_major, _version_minor, _version_patch]
